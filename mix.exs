8ccb9b3dbc492ba09bb5d05a0315b6c7df3c1ba6
Steve Newell <steve.newell@mx.com>
2017-11-07 20:30:06 -0700

    add ex_guard

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule AuthenticationKata.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ex_guard, "~> 1.3", only: :dev}
     ]
   end
 
